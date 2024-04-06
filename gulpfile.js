968900763b57f5f309a9c921533ee3a1b6e5f9f9
Nikodem Szołtysek <48836987+XNizi@users.noreply.github.com>
2024-04-06 14:11:41 +0200

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+const { src, dest, series, parallel, watch } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const cssnano = require('gulp-cssnano')
+const autoprefixer = require('gulp-autoprefixer')
+const rename = require('gulp-rename')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const imagemin = require('gulp-imagemin')
+const sourcemaps = require('gulp-sourcemaps')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
+const clean = require('gulp-clean')
+const kit = require('gulp-kit')
+
+const paths = {
+	dist: './dist',
+   html: './html/**/*.kit',
+	sass: './src/sass/**/*.scss',
+	sassDest: './dist/css',
+	js: './src/js/**/*.js',
+	jsDest: './dist/js',
+	img: './src/img/*',
+	imgDest: './dist/img',
+}
+
+function sassCompiler(done) {
+	src(paths.sass)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(cssnano())
+		.pipe(
+			rename({
+				suffix: '.min',
+			})
+		)
+		.pipe(sourcemaps.write())
+		.pipe(dest(paths.sassDest))
+	done()
+}
+
+function javascript(done) {
+	src(paths.js)
+		.pipe(sourcemaps.init())
+		.pipe(
+			babel({
+				presets: ['@babel/env'],
+			})
+		)
+		.pipe(uglify())
+		.pipe(
+			rename({
+				suffix: '.min',
+			})
+		)
+		.pipe(sourcemaps.write())
+		.pipe(dest(paths.jsDest))
+	done()
+}
+
+function convertImage(done) {
+	src(paths.img).pipe(imagemin()).pipe(dest(paths.imgDest))
+	done()
+}
+
+function handleKits(done) {
+	src(paths.html)
+   .pipe(kit())
+   .pipe(dest('./'))
+   done()
+
+}
+
+function cleanStuff(done) {
+	src(paths.dist, { read: false }).pipe(clean())
+	done()
+}
+
+function startBrowserSync(done) {
+	browserSync.init({
+		server: {
+			baseDir: './',
+		},
+	})
+	done()
+}
+
+function WatchForChanges(done) {
+	watch('./*.html').on('change', reload)
+	watch([paths.html, paths.sass, paths.js], parallel(handleKits, sassCompiler, javascript)).on('change', reload)
+	watch(paths.img, convertImage).on('change', reload)
+	done()
+}
+
+exports.cleanStuff = cleanStuff
+const mainFunctions = parallel(handleKits, sassCompiler, javascript, convertImage)
+exports.default = series(mainFunctions, startBrowserSync, WatchForChanges)
